5185f3a2caeda71019fc1453231a819b63d4fcd9
Jabbrowocky <mindbull3tz@gmail.com>
2018-02-15 20:09:18 -0600

    initial site template underway

---
+++ AlchemyAsp/AlchemyAsp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlchemyAsp.Startup))]
+namespace AlchemyAsp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
